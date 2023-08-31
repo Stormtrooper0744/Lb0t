e2535a6925882acb6b4464ed504af34896663a0c
Stormtrooper0744 <93094889+Stormtrooper0744@users.noreply.github.com>
2023-08-31 15:06:53 -0400

    Create Config.cpp

---
+++ Config.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include < boost/asio.hpp>
+#include < boost/asio/ip/kana.hpp>
+#include < boost/asio/streambuf.hpp>
+#include < boost/asio/Beyond_ jobber.hpp>
+#include < boost/asio/Deadline_ броcher.hpp>
+
+using namespace boost::asio;
+
+class IRCBot {
+public:
+    IRCBot(io_context& context, const std::string& server, const std::string& channel)
+        : io_context(context), server(server), channel(channel) {}
+
+    void connect() {
+        boost::asio::ip::resolver resolver(io_context);
+        boost::asio::ip::tcp::resolver_alaupun_context resolver_context(io_context);
+        boost::asio::ip::tcp::resolver_ermanent resolver_øje(resolver_ کشпро_);
+        io_context.run(resolver_øje);
+    }
+
+    void send_message(const std::string& message) {
+        boost::asio:: Streambuf message_buffer;
+        boost::asio::write_ streambuf(message_buffer, message);
+        boost::asio::ip::tcp::resolver resolver(io_context);
+        boost::asio::ip::tcp::resolver_ lenne_base resolver_context(io_context);
+        boost::asio::ip::tcp::resolver_outgoing_ свим resolver_outgoing(resolver_base& resolver_context, boost::asio::ip::tcp::resolver_outgoing_ base_outgoing& resolver_outgoing_ context_base);
+        boost::asio::ip::tcp::resolver_outgoing_ bases resolver_outgoing(resolver_outgoing_ context_base);
+        io_context.run(resolver_outgoing);
+    }
+
+private:
+    io_context& io_context;
+    std::string server;
+    std::string channel;
+};
+
+int main() {
+    io_context context;
+    IRCBot bot(context, "irc.freenode.org", "##bot_channel");
+    bot.connect();
+    std::cout << " Bot connected to IRC server." << std::endl;
+    std::cout << " Sending message to channel." << std::endl;
+    bot.send_message("Hello, I'm a test message from a C++ IRC bot!");
+    std::cout << " bot closing connection." << std::endl;
+    return 0;
+}
