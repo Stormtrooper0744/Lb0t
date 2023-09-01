756c99bc78e3316109eaee1cd0c36ff835105877
Stormtrooper0744 <93094889+Stormtrooper0744@users.noreply.github.com>
2023-09-01 12:05:38 -0400

    Update Config.cpp

--- Config.cpp
+++ Config.cpp
@@ -38,7 +38,7 @@ private:
 
 int main() {
     io_context context;
-    IRCBot bot(context, "irc.freenode.org", "##bot_channel");
+    IRCBot bot(context, "irc.freenode.net", "##bot_channel");
     bot.connect();
     std::cout << " Bot connected to IRC server." << std::endl;
     std::cout << " Sending message to channel." << std::endl;
